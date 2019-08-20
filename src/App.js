2743a7a3125a83cfdb6e9780a9cc68b31e90a20e
Ty Wood <twood06@gmail.com>
2019-08-20 17:58:12 -0400

    tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+	
+
+	render() {
+		return (
+			<div>
+        test
+        <Button bsStyle="primary">Primary</Button>
+      </div>
+		);
+	}
 }
 
 export default App;
