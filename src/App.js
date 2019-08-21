a84c9b8c1907114272e249a282b828f6787b8def
Ty Wood <twood06@gmail.com>
2019-08-21 00:42:05 -0400

    working on buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header'
 
 
 class App extends Component {
-	
 
 	render() {
 		return (
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,34 @@
-import React, {Component} from 'react'
-import Button from '../Button'
-import './style.css'
-
+import React, { Component } from 'react';
+import Button from '../Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './style.css';
 
 class Header extends Component {
-    render() {
-        return(
-            <Button>Test</Button>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isHidden: true
+		};
+		this.toggleHidden = this.toggleHidden.bind(this);
+	}
+
+	toggleHidden() {
+		this.setState((prevState) => ({ isHidden: !prevState.isHidden }));
+	}
+
+	render() {
+		return (
+			<Row className="headerTesting">
+				<Col sm={8}>Test</Col>
+				<Col sm={4}>
+					<Button onClick={this.toggleHidden} className="logIn">
+						{this.state.isHidden ? "Log In" : "Logoff"}
+					</Button>
+				</Col>
+			</Row>
+		);
+	}
 }
 
-export default Header
+export default Header;
